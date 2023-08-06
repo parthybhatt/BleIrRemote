3fd8fb94a1df50884395dce83b3dfb0c8f2d0dd4
parthybhatt <44039121+parthybhatt@users.noreply.github.com>
2023-08-06 11:09:50 -0500

    Adding battery manager module
    Battery manager module to measure battery voltage on seeed studio board

---
+++ ir_tv_remote/src/battery_manager/battery_manager.c
@@ -0,0 +1,129 @@
+/********************************************************************************
+* Filename    : battery_manager.c
+
+* Author      : Parth Bhatt
+
+* Date        : May 17, 2023
+
+* Description : 
+
+********************************************************************************/
+
+/*******************************************************************************
+* Include Files
+********************************************************************************/
+#include <stdint.h>
+#include "battery_manager.h"
+#include "nrf_gpio.h"
+#include "nrfx_saadc.h"
+
+/*******************************************************************************
+* Constants and macros
+********************************************************************************/
+#define BATTERY_MEAS_ADC_RESOLUTION          NRF_SAADC_RESOLUTION_10BIT
+
+// expansion values of the NRF_SAADC_RESOLUTION macros are 0,1,2,3 for 8bit, 10bit,
+// 12bit and 14bit respectively. Hence, to get the number of bits, we use the following
+// macro - NUM_BITS_IN_ADC_OUTPUT 
+#define NUM_BITS_IN_ADC_OUTPUT              (2 * (BATTERY_MEAS_ADC_RESOLUTION + 4))
+#define MAX_SAMPLE_VAL                      (1 << NUM_BITS_IN_ADC_OUTPUT)
+
+#define VOLTAGE_DIVIDER_RH_KOHMS             (1000.0f)
+#define VOLTAGE_DIVIDER_RL_KOHMS             (510.0f)
+#define BATTERY_VOLTAGE_CALC_SCALE_FACTOR   (\
+                VOLTAGE_DIVIDER_RL_KOHMS / \
+                (VOLTAGE_DIVIDER_RH_KOHMS + VOLTAGE_DIVIDER_RL_KOHMS) \
+                                            )
+#define VREF                                (3.3f)
+
+/*******************************************************************************
+* Data types (Structs, enum)
+********************************************************************************/
+
+/*******************************************************************************
+* Static Variables
+********************************************************************************/
+static uint32_t batt_enable_pin = NRF_GPIO_PIN_MAP(0,14);
+static int16_t  last_sample_val = 0xFFFF;
+
+/*******************************************************************************
+* Static Functions Declaration
+********************************************************************************/
+static void AdcConversionCompleteCallback(nrfx_saadc_evt_t const * p_event);
+
+/*******************************************************************************
+* Public Functions
+********************************************************************************/
+
+void BatteryManager_Init()
+{
+    nrf_gpio_cfg_output(batt_enable_pin);
+    nrf_gpio_pin_clear(batt_enable_pin);
+
+    nrfx_saadc_config_t sdaac_config = 
+    {
+        .resolution = BATTERY_MEAS_ADC_RESOLUTION,
+        .oversample = NRF_SAADC_OVERSAMPLE_DISABLED,
+        .interrupt_priority = 6,
+        .low_power_mode = true,
+    };
+    nrfx_saadc_init(&sdaac_config, AdcConversionCompleteCallback);
+    
+    nrf_saadc_channel_config_t channel_config = 
+    {
+      .resistor_p = NRF_SAADC_RESISTOR_DISABLED,      
+      .resistor_n = NRF_SAADC_RESISTOR_DISABLED,      
+      .gain       = NRF_SAADC_GAIN1_4,                
+      .reference  = NRF_SAADC_REFERENCE_VDD4,     
+      .acq_time   = NRF_SAADC_ACQTIME_10US,           
+      .mode       = NRF_SAADC_MODE_SINGLE_ENDED,      
+      .burst      = NRF_SAADC_BURST_DISABLED,         
+      .pin_p      = (nrf_saadc_input_t)(NRF_SAADC_INPUT_AIN7),       
+      .pin_n      = NRF_SAADC_INPUT_DISABLED          
+    };
+    nrfx_saadc_channel_init(7, &channel_config);        
+
+    ret_code_t err_code;
+    err_code = nrfx_saadc_buffer_convert(&last_sample_val, 1);
+
+}
+
+void BatteryManager_SampleBattery()
+{
+    nrfx_err_t err = nrfx_saadc_sample();
+}
+
+float BatteryManager_GetLevelVolts()
+{
+    if(last_sample_val >= 0)
+    {
+        float adcV = VREF * last_sample_val / MAX_SAMPLE_VAL;
+        float scale = BATTERY_VOLTAGE_CALC_SCALE_FACTOR;
+        return (adcV / scale);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int16_t BatteryManager_GetLevelADCCounts()
+{
+    return last_sample_val;
+}
+
+/*******************************************************************************
+* Static Functions
+********************************************************************************/
+static void AdcConversionCompleteCallback(nrfx_saadc_evt_t const * p_event)
+{
+    if(p_event->type == NRFX_SAADC_EVT_DONE)
+    {
+        ret_code_t err_code;
+
+        err_code = nrfx_saadc_buffer_convert(p_event->data.done.p_buffer, 1);
+        APP_ERROR_CHECK(err_code);
+
+        last_sample_val = p_event->data.done.p_buffer[0];
+    }
+}
